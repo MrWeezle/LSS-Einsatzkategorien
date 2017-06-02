b4ada86d7c0475503ec545ae5f526879d91a4bf1
Simon W <MrWeezle@users.noreply.github.com>
2017-06-02 16:27:06 +0200

    Update lss-ffinningen.user.js

--- lss-ffinningen.user.js
+++ lss-ffinningen.user.js
@@ -2,7 +2,7 @@
 // @name        Einsatzkategorien
 // @namespace   Leitstellenspiel
 // @include     http*://www.leitstellenspiel.de/*
-// @version     0.3.1.2
+// @version     0.3.1.3
 // @author      FFInningen
 // @grant       GM_setValue
 // @grant       GM_getValue
