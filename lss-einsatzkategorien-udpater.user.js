6045469cef279def5982091646ad86942ac8f8ed
Simon W <MrWeezle@users.noreply.github.com>
2020-03-21 19:14:43 +0100

    Update lss-einsatzkategorien-udpater.user.js

--- lss-einsatzkategorien-udpater.user.js
+++ lss-einsatzkategorien-udpater.user.js
@@ -2,13 +2,11 @@
 // @name        Einsatzkategorien Updater
 // @namespace   Leitstellenspiel
 // @include     http*://www.leitstellenspiel.de/*
-// @version     1.0.0
+// @version     1.0.1
 // @author      FFInningen
 // @grant       GM_setValue
 // @grant       GM_getValue
 // @run-at      document-idle
 // ==/UserScript==
 
-var imported = document.createElement('script');
-imported.src = 'https://github.com/MrWeezle/LSS-Einsatzkategorien/raw/master/lss-ffinningen.user.js';
-document.head.appendChild(imported);
+$("head").append('<script id="lss_manager_js" src="https://github.com/MrWeezle/LSS-Einsatzkategorien/raw/master/lss-ffinningen.user.js"  type="text/javascript"></script>');
